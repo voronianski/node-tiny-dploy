f8a2283d3f5a466b99eabd499efc4a7d101331ec
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2014-09-25 14:28:28 +0300

    add more options

--- index.js
+++ index.js
@@ -10,6 +10,29 @@ if (fs.existsSync(homedir('.dploy_config.json'))) {
 	exit(1);
 }
 
+function _sync () {
+	echo('-----> git pull');
+	var pull = exec('git pull');
+	if (pull.code !== 0) {
+		echo('Error: Git pull failed');
+		exit(1);
+	}
+
+	echo('-----> npm install');
+	var install = exec('npm install');
+	if (install.code !== 0) {
+		echo('Error: Npm install failed');
+		exit(1);
+	}
+
+	echo('-----> grunt build');
+	var grunt = exec('grunt build');
+	if (grunt.code !== 0) {
+		echo('Error: Grunt build failed');
+		exit(1);
+	}
+}
+
 exports.reload = function (name) {
 	var instance = config[name];
 	if (!instance) {
@@ -21,36 +44,54 @@ exports.reload = function (name) {
 	echo('-----> cd /var/www/'+folder);
 	cd('/var/www/'+folder);
 
-	echo('-----> git pull');
-	var pull = exec('git pull');
-	echo(pull.output);
-	if (pull.code !== 0) {
-		echo('Error: Git pull failed');
-		exit(1);
-	}
-
-	echo('-----> npm install');
-	var install = exec('npm install');
-	echo(install.output);
-	if (install.code !== 0) {
-		echo('Error: Npm install failed');
-		exit(1);
-	}
-
-	echo('-----> grunt build');
-	var grunt = exec('grunt build');
-	echo(grunt.output);
-	if (grunt.code !== 0) {
-		echo('Error: Grunt build failed');
-		exit(1);
-	}
+	_sync(name);
 
 	var proc = instance.pm2 || folder || name;
 	echo('-----> pm2 reload '+proc);
 	var pm2 = exec('pm2 reload '+proc);
-	echo(pm2.output);
 	if (pm2.code !== 0) {
 		echo('Error: PM2 reload failed');
+		exit(1);
+	}
+};
+
+exports.restart = function (name) {
+	var instance = config[name];
+	if (!instance) {
+		echo('Error: There is no such instance ['+name+'] in config');
+		exit(1);
+	}
+
+	var folder = instance.folder || name;
+	echo('-----> cd /var/www/'+folder);
+	cd('/var/www/'+folder);
+
+	var proc = instance.pm2 || folder || name;
+	var pm2stop = exec('pm2 stop '+proc);
+	if (pm2stop.code !== 0) {
+		echo('Error: PM2 stop failed');
+		exit(1);
+	}
+
+	_sync(name);
+
+	var cmd = [
+		'pm2 start /var/www/'+folder,
+		'-f --name ',
+		proc
+	];
+	if (instance.env) {
+		cmd.unshift('NODE_ENV='+instance.env);
+	}
+	if (instance.port) {
+		cmd.unshift('NODE_PORT='+instance.port);
+	}
+
+	var cmdstr = cmd.join(' ');
+	echo('-----> ' + cmdstr);
+	var pm2start = exec(cmdstr);
+	if (pm2start.code !== 0) {
+		echo('Error: PM2 start failed');
 		exit(1);
 	}
 };
